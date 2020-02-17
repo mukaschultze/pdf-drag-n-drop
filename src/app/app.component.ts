7d6c0769189d1b21a9a86fb679c7fddf6e1817c4
Muka Schultze <samuelschultze@gmail.com>
2020-02-17 10:50:37 -0300

    menus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { SafeResourceUrl } from "@angular/platform-browser";
 import * as pdfMake from "pdfmake/build/pdfmake";
 import * as pdfFonts from "pdfmake/build/vfs_fonts";
-import { BehaviorSubject, Observable, Observer } from "rxjs";
-import { debounceTime, filter, map, mergeMap, pairwise, shareReplay, startWith, switchMap, take, tap } from "rxjs/operators";
+import { merge, Observable, Observer, zip } from "rxjs";
+import { debounceTime, filter, map, mapTo, pairwise, shareReplay, startWith, switchMap, take, tap } from "rxjs/operators";
 import { payload } from "../payload.json";
 import { report } from "../report.json";
 import { RootPDF } from "./elements/band";
@@ -25,8 +25,9 @@ export class AppComponent implements OnInit {
     public pdfTemplate: Observable<RootPDF>;
     public pdfDefinition: Observable<pdfMake.TDocumentDefinitions>;
     public pdfBlob: Observable<Blob>;
-    public pdfBuilding = new BehaviorSubject<boolean>(true);
     public pdfUrl: Observable<SafeResourceUrl>;
+    public buildTime: Observable<number>;
+    public building: Observable<boolean>;
 
     public viewer = localStorageSubject<"iframe" | "pdf-viewer" | "ng2-pdfjs-viewer">("pdf-viewer", "iframe");
     public theme = localStorageSubject<"dark-theme" | "light-theme">("app-theme", "dark-theme");
@@ -56,7 +57,7 @@ export class AppComponent implements OnInit {
 
         this.pdfDefinition = this.pdfTemplate.pipe(
             debounceTime(150),
-            mergeMap((template) => this.reports.generateReport(template, payload)),
+            switchMap((template) => this.reports.generateReport(template, payload)),
             shareReplay(1),
         );
 
@@ -75,6 +76,21 @@ export class AppComponent implements OnInit {
             shareReplay(1),
         );
 
+        this.buildTime = zip(
+            this.pdfTemplate.pipe(map(() => +new Date())),
+            this.pdfBlob.pipe(map(() => +new Date())),
+        ).pipe(
+            map(([start, end]) => end - start),
+            shareReplay(1),
+        );
+
+        this.building = merge(
+            this.pdfTemplate.pipe(mapTo(true)),
+            this.pdfBlob.pipe(mapTo(false)),
+        ).pipe(
+            shareReplay(1),
+        );
+
     }
 
     public buildPdf(definition: pdfMake.TDocumentDefinitions): Observable<Blob> {
@@ -85,23 +101,13 @@ export class AppComponent implements OnInit {
                 footer: (currentPage, pageCount) => `${currentPage} of ${pageCount}`,
             };
 
-            console.time("Build PDF file");
             const pdf = pdfMake.createPdf(definition);
 
             pdf.getBuffer((buffer) => {
-                console.timeEnd("Build PDF file");
-                console.log(`PDF generated with ${(buffer.byteLength / 1024 / 1024).toFixed(2)} MB`);
-
-                // const base64 = buffer.toString("base64");
-                // const dataURL = `data:application/pdf;base64, ${base64}`;
-                // this.pdfBlob = this.sanitizer.bypassSecurityTrustResourceUrl(dataURL);
-
                 const blob = new Blob([buffer], { type: "application/pdf" });
 
                 observer.next(blob);
                 observer.complete();
-
-                this.pdfBuilding.next(false);
             });
         });
     }
