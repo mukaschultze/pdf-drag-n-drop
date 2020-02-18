5189ab96ac1a7c7a4140e01f1cd7e6b9c7d29645
Muka Schultze <samuelschultze@gmail.com>
2020-02-18 10:37:18 -0300

    ngrx install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,46 +18,67 @@ import { MatTooltipModule } from "@angular/material/tooltip";
 import { MatTreeModule } from "@angular/material/tree";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { PdfViewerModule } from "ng2-pdf-viewer";
 import { PdfJsViewerModule } from "ng2-pdfjs-viewer";
+import { environment } from "../environments/environment";
 import { AppComponent } from "./app.component";
+import { AppEffects } from "./app.effects";
 import { DevDirective } from "./dev-mode.directive";
 import { PdfTreeComponent } from "./pdf-tree/pdf-tree.component";
+import { metaReducers, reducers } from "./reducers";
 import { SafePipe } from "./safe.pipe";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        PdfTreeComponent,
-        SafePipe,
-        DevDirective,
-    ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
+  declarations: [
+    AppComponent,
+    PdfTreeComponent,
+    SafePipe,
+    DevDirective,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
 
-        DragDropModule,
+    DragDropModule,
 
-        PdfJsViewerModule,
-        PdfViewerModule,
+    PdfJsViewerModule,
+    PdfViewerModule,
 
-        MatListModule,
-        MatCheckboxModule,
-        MatMenuModule,
-        MatTooltipModule,
-        MatButtonModule,
-        MatToolbarModule,
-        MatSidenavModule,
-        MatButtonToggleModule,
-        MatProgressBarModule,
-        MatExpansionModule,
-        MatInputModule,
-        MatTreeModule,
-        MatIconModule,
-        MatGridListModule,
-    ],
-    bootstrap: [AppComponent],
+    MatListModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonToggleModule,
+    MatProgressBarModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatTreeModule,
+    MatIconModule,
+    MatGridListModule,
+
+    !environment.production ?
+      StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }) :
+      [],
+
+    EffectsModule.forRoot([AppEffects]),
+
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+      },
+    }),
+
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
